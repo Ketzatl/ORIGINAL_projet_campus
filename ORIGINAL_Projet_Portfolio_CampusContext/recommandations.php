2f2475b69ecf826112409021b5499a4f36cadc16
Morgan <ketzatl@hotmail.fr>
2020-02-09 01:35:42 +0100

    - Modification du fichier recommandations.php

---
+++ ORIGINAL_Projet_Portfolio_CampusContext/recommandations.php
@@ -0,0 +1,61 @@
+<?php
+
+$bdd = new PDO('mysql:host=localhost;dbname=espace_membre', 'root', 'root');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+   <link rel="stylesheet" href="default.css">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" style="text/css" href="pictures/logoMomo.ico">
+    <title>Recommandations</title>
+    
+</head>
+
+<body>
+   
+   <header class="headerBG">
+        <p><a class="RetAccueil" href="index.html">- Retour vers Accueil -</a> </p>
+   </header>
+
+   <h1>Recommandations Professionnelles</h1>
+
+<div class="art-post-inner art-article" align="center">
+    <table>
+        <tr>
+            <th>Ajouté par : </th>
+            <td>  </td>
+        </tr>
+
+        <tr>
+            <th>Posté le : </th>
+            <td>  </td>
+        </tr>
+
+        <tr>
+            <th>Message : </th>
+            <td>  </td>
+        </tr>
+    </table>
+    <div class="cleared"></div>
+</div>
+
+
+
+
+
+
+</body>
+
+
+
+             <!-- FOOTER  -->   
+   
+    <footer class="headerBG">
+        <p class="copyMN"><b>©Morgan NOTT</b></p>
+    </footer>
+    
+</html>
